3267a025f5a8cd1b210db9bb7cd36d748bb3d3d1
isheikh22 <ibtisam.sheikh7@outlook.com>
2022-12-12 14:29:54 -0500

    Question.objects.filter(pub_date__lte=timezone.now()) returns a queryset containing Questions whose pub_date is less than or equal to - that is, earlier than or equal to - timezone.now.

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
+from django.utils import timezone
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -27,9 +28,20 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
+    """
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
+    """
+
+    def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
